9a7f5751e723d055389bf141a6df45e7cd7badab
hhypest <hypest3@yandex.ru>
2023-11-05 02:57:16 +0300

    next

--- RawNet/obj/Release/net7.0/RawNet.AssemblyInfo.cs
+++ RawNet/obj/Release/net7.0/RawNet.AssemblyInfo.cs
@@ -11,13 +11,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RawNet")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("hhNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Форк проекта RawPrinter")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RawNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RawNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/hhypest/RawNet")]
 
 // Создано классом WriteCodeFragment MSBuild.
 

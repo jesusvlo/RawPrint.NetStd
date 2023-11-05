75879c8ebee65c94b6cb4f87272b36b3a2929377
hhypest <hypest3@yandex.ru>
2023-11-05 14:02:35 +0300

    update to 1.0.1

--- RawNet/obj/Release/net7.0/RawNet.AssemblyInfo.cs
+++ RawNet/obj/Release/net7.0/RawNet.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hhNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Форк проекта RawPrinter")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RawNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RawNet")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/hhypest/RawNet")]
 
 // Создано классом WriteCodeFragment MSBuild.
